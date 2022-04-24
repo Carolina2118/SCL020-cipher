60f2b8463ec91f1c09a378bd02b74d04b67b916d
caro <loboscarolinaa@gmail.com>
2022-04-24 17:43:05 -0400

    js- llamando las variables de ingreso de datos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-const lenguajes = ["python","ruby","c#","javascript","cobol","assembly","ajax","ruby","javascript","scratch","delphi","j#","perl","lisp","lua","go","haskell","tcl","sas","groovy","r","pascal","prolog",".net","javascript"];
-let totalVeces = 0;
-for (let i = 0; i < lenguajes.length; i++) {
-    console.log(lenguajes[i])
-  if (lenguajes[i] === "javascript") {
-    totalVeces++;
-  }
-}
-alert("La cantidad de veces que aparece javascript es: " + totalVeces)
+/*port cipher from './cipher.js';
+console.log(cipher);*/
 
+var numero = document.getElementById("numeroCard");
+var nombre = document.getElementById("nombreCard");
+var expiracion1 = document.getElementById("expireCardMes");
+var expiracion2 = document.getElementById("expireCardyear");
+
+var from = document.getElementById("formularioCard")
